65c56541e67bf32404bb0381319dc211388f8960
Georg Wallmann <gewaelbe@gmail.com>
2021-07-06 16:57:59 +0200

    pypi setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # py-lmd
+
+The py-lmd library allows to read, modify and write shape files for the Leica LMD6 & LMD7 microscope.
+Additionally, functionality for the calibration, generation of final pulses and import of vektor files is included.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-lmd",
+    version="0.1.0",
+    author="Georg Wallmann",
+    author_email="g.wallmann@campus.lmu.de",
+    description="Read, Modify and Create new shape files for the Leica LMD6 & LMD7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GeorgWa/py-lmd",
+    project_urls={
+        "Bug Tracker": "https://github.com/GeorgWa/py-lmd/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
