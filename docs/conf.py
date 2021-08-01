a8436d94ebc44e2db64fe50073770217b8dec1ed
Georg Wallmann <gewaelbe@gmail.com>
2021-08-01 18:49:20 +0200

    updated sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -71,3 +71,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_elements = {
+  'extraclassoptions': 'openany,oneside'
+}
