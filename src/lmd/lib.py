44620c804c93a4536c21b4ca92ecbba01ed3d31c
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-06-10 09:27:38 +0200

    [Refactor] Update error message

--- src/lmd/lib.py
+++ src/lmd/lib.py
@@ -601,7 +601,9 @@ class Shape:
         # Assert correct dimensions
         point_shapes = points.shape
         if (point_shapes.ndim != 2) or (point_shapes[1] != 2):
-            raise ValueError("please provide a numpy array of shape (N, 2)")
+            raise ValueError(
+                f"Shape {name}: Shape dimensionality is not valid. Please provide a numpy array of shape (N, 2)"
+            )
 
         if len(points) < 3:
             raise ValueError(
