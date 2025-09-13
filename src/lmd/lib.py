eddb9ea15fbcc41974a5b56faff2d5f0f291644f
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-09-13 15:28:02 +0200

    [FIX] try and fix plotting of centers

--- src/lmd/lib.py
+++ src/lmd/lib.py
@@ -1100,7 +1100,7 @@ class SegmentationLoader:
             if len(center) > 1:
                 axs.scatter(center[:, 1], center[:, 0], s=1)
             else:
-                axs.scatter(center[1], center[0], s=1)
+                axs.scatter(center[0][1], center[0][0], s=1)
 
             for shape in polygons:
                 axs.plot(shape[:, 1], shape[:, 0], color="red", linewidth=1)
