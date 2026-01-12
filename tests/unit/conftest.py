5b6febda684d20edae367d6821d72c44c62a7462
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-01-13 00:18:07 +0100

    Remove NJIT flag as the compilation fails in CI/CD

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,5 @@
-import os
-
 import matplotlib
 import matplotlib.pyplot
-
-# Disable jit so that the coverage is correctly reported
-os.environ["NUMBA_DISABLE_JIT"] = "1"
 
 matplotlib.use("Agg")
 matplotlib.pyplot.ioff()
